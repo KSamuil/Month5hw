8525a0fdafb95fc0cefccced4328a316ae452588
KSamuil <ksuharev49@gmail.com>
2023-12-07 21:24:46 +0600

    hw2

--- afisha/movie_app/serializers.py
+++ afisha/movie_app/serializers.py
@@ -1,38 +1,52 @@
 from rest_framework import serializers
 from movie_app.models import Director, Movie, Review
-
-
 class DirectorSerializer(serializers.ModelSerializer):
     class Meta:
         model = Director
         fields = 'director'
 
 
-class DirDetailSerializer(serializers.ModelSerializer):
+class DirectorDetailSerializer(serializers.ModelSerializer):
     class Meta:
         model = Director
-        fields = '__all__'
+        fields = 'id', 'name'
 
 
 class MovieSerializer(serializers.ModelSerializer):
+    rating = serializers.SerializerMethodField()
+
     class Meta:
         model = Movie
-        fields = 'title', 'description', 'duration'
+        fields = ('id', 'title', 'description', 'duration', 'director', 'rating')
+
+        def get_rating(self, obj):
+            review_all = obj.movie_review.all()
+            rate = [i.rate_stars for i in review_all]
+            rate_avg = sum(rate) / len(rate)
+            return rate_avg
 
 
 class MovieDetailSerializer(serializers.ModelSerializer):
     class Meta:
         model = Movie
         fields = '__all__'
-
-
 class ReviewSerializer(serializers.ModelSerializer):
     class Meta:
         model = Review
-        fields = "__all__"
+        fields = ('id', 'text', 'rate_stars')
 
 
 class ReviewDetailSerializer(serializers.ModelSerializer):
     class Meta:
         model = Review
-        fields = "__all__"
+        fields = ('id', 'text', 'movie')
+
+    class DirectorSerializer(serializers.ModelSerializer):
+        movie_count = serializers.SerializerMethodField()
+
+        class Meta:
+            model = Director
+            fields = ('id', 'name', 'movie_count')
+
+        def get_movie_count(self, obj):
+            return obj.movie_director.count()
--- afisha/movie_app/views.py
+++ afisha/movie_app/views.py
@@ -12,38 +12,29 @@ def get_director_list(request):
     director = Director.objects.all()
     serializer = DirectorSerializer(director, many=True)
     return Response(serializer.data)
-
-
 @api_view(['GET'])
 def get_director_by_id(request, dir_id):
     director = Director.objects.get(id=dir_id)
     serializer = Director(director)
     return Response(serializer.data)
-
-
 @api_view(['GET'])
 def get_movie_list(request):
     movie = Movie.objects.all()
     serializer = MovieSerializer(movie, many=True)
     return Response(serializer.data)
-
-
 @api_view(['GET'])
 def get_movie_by_id(request, movie_id):
     movie = Movie.objects.get(id=movie_id)
     serializer = Movie(movie)
     return Response(serializer.data)
-
-
 @api_view(['GET'])
 def get_review_list(request):
     review = Review.objects.all()
     serializer = ReviewSerializer(review, many=True)
     return Response(serializer.data)
 
-
 @api_view(['GET'])
 def get_review_by_id(request, review_id):
-    review = Review.objects.get(id=review_id)
+    review = Review.objects.filter(id=review_id)
     serializer = Review(review)
     return Response(serializer.data)
